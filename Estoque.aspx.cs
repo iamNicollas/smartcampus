bff79bebbbecd3377ffbe5eaca06f9fcaf3eabf9
vanderson <vandersonaparecido15@gmail.com>
2025-10-22 20:55:17 -0300

    first commit update develop

--- Estoque.aspx.cs
+++ Estoque.aspx.cs
@@ -11,6 +11,7 @@ namespace smartCampos
 {
     public partial class Estoque : System.Web.UI.Page
     {
+        methodProduto method = new methodProduto();
         protected void Page_Load(object sender, EventArgs e)
         {
             if(!IsPostBack)
@@ -21,12 +22,11 @@ namespace smartCampos
 
         private void CarregarProdutos()
         {
-            methodProduto method = new methodProduto();
-            DataTable dt = method.ListarProduto();
+            List<objProduto> listaProdutos = method.ListarProduto();
 
-            if (dt != null && dt.Rows.Count > 0)
+            if (listaProdutos != null && listaProdutos.Count > 0)
             {
-                grd.DataSource = dt;
+                grd.DataSource = listaProdutos;
                 grd.DataBind();
             }
             else
